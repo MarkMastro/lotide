fee727e85d523805adf35ec1063e528bf6375121
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-07-25 14:58:42 +0000

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+      return;
+    }
+  } console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  return;
+};
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
