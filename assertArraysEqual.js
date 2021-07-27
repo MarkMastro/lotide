236197fcf2d4c81727851d16cda4ac2947ba8878
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-07-27 23:16:43 +0000

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
@@ -7,10 +7,23 @@ const eqArrays = function(actual, expected) {
   } console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
   return;
 };
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  } return true;
+};
 
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
