c371a5698e94192532d812f297639b949719249e
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-07-31 01:37:33 +0000

    letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i=0;i<sentence.length ; i++){
+   
+    if(sentence[i]==' '){
+      //skip
+    }else if(results[sentence[i]]==undefined){
+      
+      results[sentence[i]]=[];
+      results[sentence[i]].push(i);
+    }else{
+      results[sentence[i]].push(i)
+    }
+  }
+  // logic to update results here
+  return results;
+};
+
+console.log(letterPositions('Complete Assignment'))
