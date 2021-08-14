3f4622f735871bf9a347eadbfa0cb810db341408
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-14 20:47:24 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -9,10 +9,5 @@ const eqArrays = function(actual, expected) {
     return false;
   }
 };
+module.exports=eqArrays
 
-// TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
