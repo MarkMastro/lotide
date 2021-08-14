07ba7fc97c9b702862ab066f1d0688c8f2a2662f
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-14 20:03:35 +0000

    updated head to use require/export

--- head.js
+++ head.js
@@ -1,14 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  }
-  console.assert(actual === expected,  ` 🛑🛑🛑 ${actual} !== ${expected} `);
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assertEqual = require('./assertEqual');
 
 function head(arr){
   return arr[0]
 }
+module.exports=head;
