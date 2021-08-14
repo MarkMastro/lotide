a61f09b730a4b7901d2b659aebcd99056d0e5af3
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-14 20:29:24 +0000

    tests

---
+++ tests/tailTest.js
@@ -0,0 +1,4 @@
+const tail=require('../tail')
+const assertArraysEqual=require('../assertArraysEqual')
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertArraysEqual(result, ["Lighthouse", "Labs"]);
