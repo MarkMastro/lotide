15ff135a42d1b8a88c2701d34c0498668f694216
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-14 21:35:00 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,13 +1,3 @@
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-      return;
-    }
-  } console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  return;
-};
-
 const middle=function(array){
  
   if(array.length % 2 === 0 && array.length>2){//even length array
@@ -22,28 +12,7 @@ const middle=function(array){
   }
 }
 
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  } return true;
-};
+module.exports=middle;
 
-// TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-console.log(middle([1,2]));
-console.log(middle([]));
-console.log(middle([1,2,3,4,5,6,7]));
-
-// TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
