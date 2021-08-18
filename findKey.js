7e200d88b137bdc829aa37f19f393946730031da
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-18 00:12:34 +0000

    updated to return first instance of matching key, not array of all matches

--- findKey.js
+++ findKey.js
@@ -3,12 +3,11 @@ const findKey = function(obj,callback) {
   let results = [];
   for (const key in obj) {
     if (callback(obj[key])) {
-      results.push(key);
+      return key
     } else {
       continue;
     }
   }
-  console.log(results);
   return results;
 };
 module.exports = findKey;
